2fd99bca5e634b9c279f753129dc379ddf35d52d
ssharma454 <ssharma454@gatech.edu>
2020-02-16 19:27:54 -0500

    Fixed resuming functionality of quast

--- quast_wrapper.py
+++ quast_wrapper.py
@@ -65,10 +65,11 @@ def quast_runner(output_directory_path):
 			os.mkdir(output_dir_path)
 
 	#Get files that have already been processed.
-	with open('tmp/quast_outputs.txt') as f:
+	with open('tmp/quast_completed.txt') as f:
 		raw = f.read()
 
 	already_processed_files = raw.split('\n')
+	print(already_processed_files)	
 
 	for tool_name, files in all_output_files.items():
 		print("Processing files for tool: {}".format(tool_name))
@@ -88,7 +89,7 @@ def quast_runner(output_directory_path):
 			#Check if file has already been processed.
 			if input_file_path in already_processed_files:
 				print("File: {} has already been processed by Quast, skipping...".format(input_file_path))
-
+				continue
 			if not os.path.exists(file_path_for_quast):
 				os.mkdir(file_path_for_quast)
 
@@ -96,12 +97,13 @@ def quast_runner(output_directory_path):
 			#############################Execute Quast#############################
 			try:
 				print("Running Quast on: {}".format(input_file_path))
-				continue
+				
 				quast_output = subprocess.check_output(["quast", input_file_path, "-o", file_path_for_quast])
 
 				#Write the output files for later use.
-				with open('tmp/quast_outputs.txt', 'a') as f:
-					f.write(file_path_for_quast + "\n")
+				with open('tmp/quast_outputs.txt', 'a') as fo, open("tmp/quast_completed.txt", "a") as fc:
+					fo.write(file_path_for_quast + "\n")
+					fc.write(input_file_path + "\n")
 
 			except subprocess.CalledProcessError:
 				print("==========>Quast could not finish quality check for tool: {} & file: {}".format(tool_name, input_file_path))
